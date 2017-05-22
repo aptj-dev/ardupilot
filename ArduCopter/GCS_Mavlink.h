0060bb5522991cea7d71156b810e70fcfa702a84
Pierre Goudet <p-goudet@ruby-dev.jp>
2017-05-22 11:10:22 +0900

    Reformating code format

--- ArduCopter/GCS_Mavlink.h
+++ ArduCopter/GCS_Mavlink.h
@@ -21,7 +21,6 @@ protected:
     
 private:
 
-//    void handleMessage(mavlink_message_t * msg) override;
     bool handle_guided_request(AP_Mission::Mission_Command &cmd) override;
     void handle_change_alt_request(AP_Mission::Mission_Command &cmd) override;
     bool try_send_message(enum ap_message id) override;
